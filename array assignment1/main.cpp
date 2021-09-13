2db3ccddfabb05bd8b4e49b01ec5b6d8ef132a4d
SloaneT <89420377+SloaneT@users.noreply.github.com>
2021-09-13 13:56:40 -0700

    array swap

--- array assignment1/main.cpp
+++ array assignment1/main.cpp
@@ -3,10 +3,11 @@ using namespace std;
 
 int main()
 {
-	const int n = 10;
-	int numbers[n] = { 5,7,2,4,6,7,10,1,3,9};
+    int n = 10;
+	int numbers[n] = {5,7,2,4,6,7,10,1,3,9};
 
-	for(int i=1; i<n-1; i++ )
+
+	for(int i=1; i<n-1; i++)
 	{
 		if ( numbers[i-1] < numbers[i] &&  numbers[i] < numbers[i+1])
 			cout << numbers[i] << "is in increasing order. \n";
