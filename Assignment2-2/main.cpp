054146e109a5d1510b141a64fc8dda451366c334
SloaneT <89420377+SloaneT@users.noreply.github.com>
2021-09-29 14:11:49 -0700

    Assignment2-2

--- Assignment2-2/main.cpp
+++ Assignment2-2/main.cpp
@@ -7,7 +7,6 @@ int main()
 {
     const int N = 10;
     double numbers[N] = {10.0, 15.2, 21.2, 10.5, 11.3, 12.5, 13.8, 14.7, 19.8, 20.0};
-    double * const n = numbers;
 
     printArray(numbers, N);
 }
