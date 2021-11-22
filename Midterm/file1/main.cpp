93e8e85ac92964e22f88cb8f530012f7ef1131dc
SloaneT <89420377+SloaneT@users.noreply.github.com>
2021-11-22 11:43:42 -0800

    question1 completed

--- Midterm/file1/main.cpp
+++ Midterm/file1/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    queue<int> q; //Declares a queue of integers named "q"
+
+for (int i = 0; i < 5; i++) //Declares a for loop that pushes values
+	q.push(i); // 0 through 4 into queue
+
+cout << "The size of q " << q.size() << endl; //Size of queue is 5 integers or 20 bytes
+
+cout << " Dequeued : " << q.front() << endl; //Displays the item at the front of the queue, which is 0
+
+q.pop(); //Removes first element in the queue, 0
+
+cout << " Dequeued : " << q.front() << endl; //Displays the new front, which is 1
+
+q.pop();
 }
