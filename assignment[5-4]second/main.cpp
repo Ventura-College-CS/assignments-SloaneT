e2ee34e47e1d8a835eff6094eab4fca8ed3b5fc8
SloaneT <89420377+SloaneT@users.noreply.github.com>
2021-11-24 13:44:34 -0800

    file5

--- assignment[5-4]second/main.cpp
+++ assignment[5-4]second/main.cpp
@@ -17,30 +17,23 @@ class Student {
             sname = "";
             score[0] = 0;
             score[1] = 0;
-            score[3] = 0;
+            score[2] = 0;
 		}
 		Student(int i, string s, double *sc)
 		{
 			id = i;
 			sname = s;
-			for(int i = 0; i < 3; i++)
-            {
-                score[i] = sc[i];
-            }
-//			score[0] = sc[0];
-//			score[1] = sc[1];
-//			score[2] = sc[2];
+			score[0] = sc[0];
+			score[1] = sc[1];
+			score[2] = sc[2];
 		}
 		void setValues(int i, string s, double *sc)
 		{
 			id = i;
 			sname = s;
-			for(int i = 0; i < 3; i++)
-			{
-			    score[i] = sc[0];
-			}
-//			score[0] = sc[0];
-//			score[1] = sc[1];
+			score[0] = sc[0];
+			score[1] = sc[1];
+			score[2] = sc[2];
 		}
 		int getID()
 		{
@@ -64,7 +57,7 @@ void printStudent(Student &s)
 	sc = s.getScore();
 	cout << "Score 0 " << sc[0] << endl;
 	cout << "Score 1" << sc[1] << endl;
-	cout << "Score 2" << sc[2] << endl;
+	cout << "score 2" << sc[2] << endl;
 }
 
 int main()
@@ -106,14 +99,14 @@ int main()
         cout << "Please enter name " << i + 1 << ": " << endl;
         cin >> name;
 
-        cout << "Please enter all three scores scores, one at a time for student " << i + 1 << ": " << endl;
+        cout << "Please all three scores, one at a time for student " << i + 1 << ": " << endl;
         cin >> score[0] >> score[1] >> score[2];
 
         s[i].setValues(id, name, score);
 	    studentQueue.enqueue(s[i]);
 	}
 
-	double sc[2] = {100.0, 99.9}; //creating score arrays
+	double sc[3] = {100.0, 99.9, 100.01}; //creating score arrays
 	Student s1(100, "John", sc); //enqueue a student when queue is full
 	studentQueue.enqueue(s1);
 
