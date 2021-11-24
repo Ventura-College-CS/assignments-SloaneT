bb0099a9ede9a0daec199f1975438fd6cf54fb47
SloaneT <89420377+SloaneT@users.noreply.github.com>
2021-11-24 13:29:58 -0800

    file4

--- Midterm/file4/main.cpp
+++ Midterm/file4/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <list>
 #include <queue>
+#include <deque>
 
 using namespace std;
 
 int main()
 {
-    list<int> list1(5, 100); //creates a list of 5 ints, each with the value 100
+    list<int> list1(3, 1200); //creates a list of 3 ints, each with the value 1200
 	list<int> emptyList; //creates an empty list of ints
 	list<int> list3(list1.begin(), list1.end()); //iterate through list1
 
-	queue<int, list<int> > lq(list1);
+	queue<int, list<int> > lq(list1); //declares a queue of int lists, initialized to copy of list1
+    queue<int, list<int> > emptyqueue; //empty queue with list as underlying container
 
 	cout << " Dequeued : " << lq.front() << endl;
 	lq.pop();
