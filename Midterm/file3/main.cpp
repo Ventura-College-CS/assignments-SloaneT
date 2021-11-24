ae5089c635e96a37ed212d181a0161d38ee5eef8
SloaneT <89420377+SloaneT@users.noreply.github.com>
2021-11-24 13:25:07 -0800

    file4

--- Midterm/file3/main.cpp
+++ Midterm/file3/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#include <deque>
+#include <deque> // initial state of empty dequeue is front&rear == -1
 using namespace std;
 
 int main()
 {
    int numbers[] = {30, 40, 10, 40, 50};
-	deque<int> dq(numbers, numbers + 4); //creates a deque of integers with range (start of numbers([-1]), numbers[-1 + 4] = numbers[3])
+	deque<int> dq(numbers, numbers + 4); //creates a deque of integers with range (start of numbers([-1]), numbers[-1 +4] = numbers[3])
 	cout << " Dequeued : " << dq.front() << endl; //Output the front of the deque using .front(), then remove the front element.
 	dq.pop_front(); //Because it's a deque, must specify whether popping front or back.
-	cout << " Dequeued : " << dq.front() << endl;
+	cout << " Dequeued : " << dq.front() << endl; // front was set to numbers[0]
 	dq.pop_front();
-	cout << " Dequeued from rear: " << dq.back() << endl; //The rear of the deque dq was set to numbers[2]
+	cout << " Dequeued from rear: " << dq.back() << endl; //The rear of the deque dq was set to numbers[3]
 	dq.pop_back();
 	cout << " Dequeued from rear: " << dq.back() << endl;
 	dq.pop_back();
--- Midterm/file4/main.cpp
+++ Midterm/file4/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <list>
+#include <queue>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    list<int> list1(5, 100); //creates a list of 5 ints, each with the value 100
+	list<int> emptyList; //creates an empty list of ints
+	list<int> list3(list1.begin(), list1.end()); //iterate through list1
+
+	queue<int, list<int> > lq(list1);
+
+	cout << " Dequeued : " << lq.front() << endl;
+	lq.pop();
+	cout << " Dequeued : " << lq.front() << endl;
+	lq.pop();
 }
