a65efed6f9a8830e949faa008a5347280a3df7da
SloaneT <89420377+SloaneT@users.noreply.github.com>
2021-12-06 16:25:01 -0800

    final

--- Final/final1/main.cpp
+++ Final/final1/main.cpp
@@ -1,21 +1,57 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Course
 {
 public:
-    Course(int ID, string name, int credits)
-    : ID(ID),name(name), credits(credits)
+    Course(int id, string name, int credits)
+    : ID(id), name(name), credits(credits)
+    {
+    }
     int ID;
     string name;
     int credits;
 
+    void printCourse()
+    {
+        cout << ID << "\t" << name << "\t" << credits << endl;
+    }
 };
 
 int main()
 {
-    Course c1;
+    Course c1(111, "Jeff", 1);
+    Course c2(132, "Steve", 1);
+    Course c3(183, "Shave", 3);
+    Course c4(214, "Dabs", 4);
+    Course c5(715, "Knobs", 2);
+    Course c6(126, "Shelves", 1);
+    Course c7(437, "Sleeves", 5);
+    Course c8(238, "Digging", 2);
+    Course c9(459, "Openers", 1);
+    Course c10(330, "Climbers", 3);
 
-    c1.setID(12);
+    vector<Course> courseVector;
+    courseVector.push_back(c1);
+    courseVector.push_back(c2);
+    courseVector.push_back(c3);
+    courseVector.push_back(c4);
+    courseVector.push_back(c5);
+    courseVector.push_back(c6);
+    courseVector.push_back(c7);
+    courseVector.push_back(c8);
+    courseVector.push_back(c9);
+    courseVector.push_back(c10);
+
+
+
+
+}
+
+void printVector(vector <Course> const &a)
+{
+    for(int i = 0; i < a.size(); i++)
+        cout << a.at(i) << endl;
 }
