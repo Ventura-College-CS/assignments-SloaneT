fe844476da9f96c9e8c9b6c7e14c8ea9b4d4ab39
SloaneT <89420377+SloaneT@users.noreply.github.com>
2021-12-07 13:48:29 -0800

    documentation

--- Final/final3/main.cpp
+++ Final/final3/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include "stack1.hpp"
+#include "Postfix.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    Postfix postfix_exp("243*+5+");
+
+    postfix_exp.evaluation();
+
+    postfix_exp.setExpression("283*+5+");
+
+    postfix_exp.printExpression();
+
 }
--- Final/final3/stack1.hpp
+++ Final/final3/stack1.hpp
@@ -1,16 +1,44 @@
-#ifndef STACK1_HPP
-#define STACK1_HPP
+#ifndef 	STACK_H //if the unique file STACK_H is not already defined
+#define 	STACK_H //define it
 
+#include <vector> //using vector STL
+using namespace std;
 
-class stack1
+template<class T, int capacity=30> //creates a template of class T, which has a capacity of 30 bytes
+class Stack
 {
-    public:
-        stack1();
-        virtual ~stack1();
 
-    protected:
+private:
+	vector<T>  pool; //creates a vector of type T called pool
+public:
+	Stack()
+	{
+		pool.reserve(capacity); //default constructor uses vector function reserve() to declare minimum capacity of 30 bytes
+	}
+	Stack(int size)
+	{
+		pool.reserve(size); //if a size is given, the Stack member uses reserve() to declare the given minimum capacity
+	}
+	T pop() //used to remove the "top" element of a stack; a vector stack treats the last element as the "top"
+	{
+		T val = pool.back(); // assigns a variable val of type T to the top element of stack
+		pool.pop_back(); // removes the last element of stack
+		return val; // returns the value which was just removed
+	}
+	void push(T v)
+	{
+		pool.push_back(v);
+	}
+	T isEmpty() const
+	{
+		return pool.empty();
+	}
+	T topEl() const
+	{
+		return pool.back();
+	}
 
-    private:
+
 };
 
-#endif // STACK1_HPP
+#endif
